d0e724fea345fda0293279bda17b6c926e970ea6
nknipe20 <nknipe20@georgefox.edu>
2023-11-10 01:44:00 -0800

    ready

--- javascriptproject/script.js
+++ javascriptproject/script.js
@@ -1,64 +1,26 @@
-document.getElementById("quiz1").addEventListener("click", function() 
-{quizQuestionOne.replaceWith(quizQuestionTwo)});
-
-var romcom_var = false;
-var drama_var = false;
-var scary_var = false;
-var comedy_var = false;
-var netflix_var = false;
-var prime_var = false;
-var hbo_var = false;
-var disney_var = false;
-
-function RomCom()
+function showWateringCan() 
 {
-    romcom_var = true;
+    var image = document.getElementById('wateringCan');
+    image.style.display = 'block';
+
+    var flower = document.getElementById('flowers')
+    flower.src = "/javascriptproject/2.png";
+
+    var wateringButton = document.getElementById('wateringCanButton');
+    wateringButton.style.display = "none";
+
+    var sunButtom = document.getElementById('sunButton');
+    wateringButton.style.display = "block";
 }
 
-function Drama()
+function showSun() 
 {
-    drama_var = true;
-}
+    var image = document.getElementById('sun');
+    image.style.display = 'block';
 
-function Scary()
-{
-    scary_var = true;
-}
+    var flower = document.getElementById('flowers')
+    flower.src = "/javascriptproject/3.png";
 
-function Comedy()
-{
-    comedy_var = true;
-}
-
-function Netflix() {
-    netflix_var = true;
-    var quiz1Buttons = document.getElementsByClassName("quiz1");
-      for (var i = 0; i < quiz1Buttons.length; i++) 
-      {
-        quiz1Buttons[i].style.display = "none";
-  }
-}
-
-function PrimeVideo()
-{
-    prime_var = true;
-} 
-
-function HBOMax()
-{
-    hbo_var = true;
-}
-
-function DisneyPlus()
-{
-    disney_var = true;
-}
- 
-
-function showResults()
-{
-    if (netflix_var && romcom_var)
-    {
-        document.getElementById('result').innerText = "Legally blonde";
-    }
+    var wateringButton = document.getElementById('SunButton');
+    wateringButton.style.display = "none";
 }
