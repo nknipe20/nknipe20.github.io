b5c03490fcb614eea2a074cad7af23f587123f94
nknipe20 <nknipe20@georgefox.edu>
2023-11-10 11:51:24 -0800

    better now

--- javascriptproject/script.js
+++ javascriptproject/script.js
@@ -11,18 +11,25 @@ function showWateringCan()
 
     var sunButton = document.getElementById('sunButton');
     sunButton.style.display = "block";
+
+    var topMessage = document.getElementById('topMessage');
+    topMessage.innerHTML = "Almost there!"
 }
 
 function showSun() 
 {
     var image = document.getElementById('sun');
     image.style.display = 'block';
+    sun.rotateImage;
 
     var flower = document.getElementById('flowers')
     flower.src = "/javascriptproject/3.png";
 
-    var wateringButton = document.getElementById('SunButton');
+    var wateringButton = document.getElementById('sunButton');
     wateringButton.style.display = "none";
+
+    var topMessage = document.getElementById('topMessage');
+    topMessage.innerHTML = "Great job! You did it!"
 }
 
 setTimeout(function() {
